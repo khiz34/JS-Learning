ca4be0545fe00d7531d89978765d47b211a5e253
khiz34 <144690200+khiz34@users.noreply.github.com>
2025-09-10 08:08:30 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,17 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3) // we are using nodejs, not browser
+
+DataTypes:
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => value not assigned
+// symbol => uniquness 
+
+
+// object 
+
+console.log(typeof null); // null is a object
