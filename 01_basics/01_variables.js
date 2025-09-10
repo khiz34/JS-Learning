81d61731ec6ed4c379a5ba4bbe76597e8731e36b
khiz34 <144690200+khiz34@users.noreply.github.com>
2025-09-10 08:10:47 +0000

    what are variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountId = 231
+let accountEmail = "khiz@google.com"
+accountCity = "karachi"
+
+accountCity = "lahore"
+accountEmail = "dinz@gmail.com"
+let accountState;
+
+console.table([accountId, accountEmail, accountCity]);
