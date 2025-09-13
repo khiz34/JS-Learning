3cdd43555da3a7cbdc80f1b933daad3c41aeee74
khiz34 <144690200+khiz34@users.noreply.github.com>
2025-09-13 11:49:55 +0000

    comparisions rules

---
+++ 01_basics/04_comparisions.js
@@ -0,0 +1,16 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+// === (strick check)
+
+// console.log("2" === 0); (it will check datatype also, is its string or number so its clean code )
+
